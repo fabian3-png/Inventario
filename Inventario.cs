17e8fe1b055cba65fe422cf059517fb655e4407a
fabian3-png <fabianramirezrosaz@gmail.com>
2026-04-16 08:01:07 -0600

    Update Inventario.cs

--- Inventario.cs
+++ Inventario.cs
@@ -1,1 +1,28 @@
+using System;
 
+public class Inventario
+
+{
+  public void AgregarProducto(nombre, stock)
+  {
+  }
+
+  public void ListarProductos()
+  {
+  }
+
+ public void  BuscarProducto(nombre)
+  {
+  }
+
+ public void  ActualizarStock(nombre)
+  {
+  }
+
+  public void EliminarProducto(id)
+  {
+  }
+
+}
+  
+  
